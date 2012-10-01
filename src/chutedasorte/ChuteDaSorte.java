0f77b547138a619897789adaa89b7a0830a9e8b6
Mateus <mateush.dalforno@gmail.com>
2012-10-01 01:02:26 -0300

    Alterações na aplicaçao

--- src/chutedasorte/ChuteDaSorte.java
+++ src/chutedasorte/ChuteDaSorte.java
@@ -1,22 +1,20 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package chutedasorte;
 
 /**
  * Classe Main que exeucta o jogo ChuteDaSorte
+ *
  * @author Mateus
  */
 public class ChuteDaSorte {
 
     /**
      * Metodo Main que executa o jogo ChuteDaSorte
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Jogo jogo = new Jogo();
         jogo.menu();
-       
+
     }
 }
--- src/chutedasorte/Jogador.java
+++ src/chutedasorte/Jogador.java
@@ -1,14 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package chutedasorte;
 
 import baralho.Carta;
-import java.util.Stack;
 
 /**
- *
+ * Classe que representa um jogador
  * @author Mateus
  */
 public class Jogador {
@@ -16,59 +11,63 @@ public class Jogador {
     private String nome;
     private int ponto;
     private Carta carta;
-    
+
     /**
      * Metodo que retorna o nome do jogador
-     * @return nome Nome do jogador
+     *
+     * @return Nome do jogador
      */
     public String getNome() {
         return this.nome;
     }
-    
+
     /**
      * Metodo que altera o nome do Jogador
+     *
      * @param nome Nome do jogador
      */
-    public void setNome(String nome){ 
+    public void setNome(String nome) {
         this.nome = nome;
     }
-    
+
     /**
      * Metodo que adiciona um ponto aos pontos do jogador
      */
     public void setUmPonto() {
         this.ponto++;
     }
-    
+
     /**
      * Metodo que adiciona dois pontos aos pontos do jogador
      */
     public void setDoisPontos() {
         this.ponto = this.ponto + 2;
     }
-    
+
     /**
      * Metido que retorna os pontos do jogador
-     * @return ponto pontos do jogador
+     *
+     * @return pontos do jogador
      */
     public int getPonto() {
         return this.ponto;
     }
-    
+
     /**
      * Metodo que Seta uma carta a ser adivinhada
+     *
      * @param carta carta a ser adivinhada
      */
-    public void setCarta(Carta carta){
+    public void setCarta(Carta carta) {
         this.carta = carta;
     }
-    
+
     /**
      * Metodo para ver o valor da carta
-     * @return
+     *
+     * @return valor
      */
-    public String verValor(){
-       return this.carta.toString();
+    public String verValor() {
+        return this.carta.toString();
     }
-
 }
